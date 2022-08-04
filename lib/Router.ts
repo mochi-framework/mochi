1fc216512ef69d57abced24ea7c39fff8e24e443
Branislav Hozza <brankohozza@gmail.com>
2022-08-04 10:57:26 +0200

    :memo: docs: add methods documentation

--- lib/Router.ts
+++ lib/Router.ts
@@ -1,10 +1,39 @@
 class Router {
   private router: object
   constructor() {}
+
+  /**
+   * Method for seting middlewares/router
+   */
   use() {}
+
+  /**
+   * Method for seting GET method handlers
+   */
   get() {}
+
+  /**
+   * Method for seting POST method handlers
+   */
   post() {}
+
+  /**
+   * Method for seting PUT method handlers
+   */
   put() {}
+
+  /**
+   * Method for seting DELETE method handlers
+   */
   delete() {}
+
+  /**
+   * Method for seting * method handlers
+   */
   all() {}
+
+  /**
+   * Method to handle current request and find corresponding handler
+   */
+  route() {}
 }
