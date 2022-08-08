bf115380b1abc59e1980b08120f4daaf72347305
Hladikes <adi.hladik@gmail.com>
2022-08-08 15:37:07 +0200

    ✨ feat: example added

--- examples/main.ts
+++ examples/main.ts
@@ -6,4 +6,26 @@ app.get('/', (req: MochiRequest, res: MochiResponse) => {
   return res.json({ x: 'y' })
 })
 
+app.get('/user', (req: MochiRequest, res: MochiResponse) => {
+  return res.json({ x: 'y' })
+})
+
+app.get('/whatever/:id', (req: MochiRequest, res: MochiResponse) => {
+  return res.json({ x: 'y' })
+})
+
+app.get(
+  '/midtest',
+  (req, res) => {
+    console.log('Middleware called')
+  },
+  (req, res) => {
+    return res.json({ oki: 'oki' })
+  },
+)
+
+app.use((req, res) => {
+  console.log('Hello World')
+})
+
 app.listen()
