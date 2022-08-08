fb492208e87fcd3dd40e7f248573f996959b9504
Hladikes <adi.hladik@gmail.com>
2022-08-08 11:00:44 +0200

    🚧 feat: route mapper added

--- lib/Router.ts
+++ lib/Router.ts
@@ -1,7 +1,65 @@
-import { Endpoint, Middleware, RouteMap } from './types'
+import { Endpoint, Method, Middleware, RouteMap, Route, HandlerType } from './types'
 
 export class Router {
   private router: RouteMap
+
+  constructor() {}
+
+  private setupPath(path: string, handler: Middleware | Endpoint | Router, type: HandlerType, method: Method): void {
+    const parts = path.split('/')
+    let current = this.router
+
+    for (const part of parts) {
+      if (part === '') {
+        continue
+      }
+
+      const isParam = part.startsWith(':')
+      const targetPath = isParam ? ':' : part
+      const targetRoute: Route = {
+        children: new Map(),
+      }
+
+      if (isParam) {
+        targetRoute.paramName = part.substring(1)
+      }
+
+      current.set(targetPath, targetRoute)
+      current = current.get(targetPath).children
+    }
+
+    const route = current.get('') ?? { [method]: {} }
+
+    if (type === HandlerType.ENDPOINT) {
+      route[method].handler = handler as Endpoint
+      return
+    }
+
+    if (type === HandlerType.MIDDLEWARE) {
+      if (!Array.isArray(route[method].middlewares)) {
+        route[method].middlewares = []
+      }
+
+      route[method].middlewares.push(handler as Middleware)
+      current.set('', route)
+      return
+    }
+
+    if (type === HandlerType.ROUTER) {
+      current.set('', {
+        ...route,
+        router: handler as Router,
+      })
+      return
+    }
+  }
+
+  private processHandlers(method: Method, path: string, handlers: [...Middleware[], Endpoint]) {
+    for (let idx in handlers) {
+      const handlerType = Number(idx) === handlers.length - 1 ? HandlerType.ENDPOINT : HandlerType.MIDDLEWARE
+      this.setupPath(path, handlers[idx], handlerType, method)
+    }
+  }
 
   /**
    * Method for setting middlewares/router
