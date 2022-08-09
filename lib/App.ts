43ccbfd1e57ac8c94fa2ce0481215950afe62069
Hladikes <adi.hladik@gmail.com>
2022-08-09 09:37:10 +0200

    :bricks: feat: listen now returns the server instance

--- lib/App.ts
+++ lib/App.ts
@@ -1,15 +1,26 @@
+import { Server } from 'bun'
 import { Router } from './Router'
+import { Config } from './types'
 
 export class App extends Router {
-  constructor(private config: object = {}) {
+  constructor(private config: Config) {
     super()
   }
 
   /**
    * Method to start app and listen to incoming requests
    */
-  listen() {
+  listen(): Server {
     //TODO: Implement server listener
-    console.log(this.router)
+    const server = Bun.serve({
+      fetch: (req: Request): Response => {
+        const path = '/' + req.url.replace(server.hostname, '')
+        return new Response(`Thank you for visiting ${path}`)
+      },
+      port: this.config.port,
+      ...(this.config.host ? { hostname: this.config.host } : {}),
+    })
+
+    return server
   }
 }
