b9005d66f81124530419e451dfe34087725f6fca
Hladikes <adi.hladik@gmail.com>
2022-08-09 09:37:56 +0200

    :sparkles: feat: example updated

--- examples/main.ts
+++ examples/main.ts
@@ -1,6 +1,8 @@
 import { App, MochiRequest, MochiResponse } from '../lib'
 
-const app = new App()
+const app = new App({
+  port: 3000,
+})
 
 app.get('/', (req: MochiRequest, res: MochiResponse) => {
   return res.json({ x: 'y' })
@@ -28,4 +30,5 @@ app.use((req, res) => {
   console.log('Hello World')
 })
 
-app.listen()
+const server = app.listen()
+console.log(`[i] Server is running at ${server.hostname}`)
